c93ae27a408bf0d9f719fa6dc3c5822bd6202d78
Sammy Alvarado <isammyalvarado@gmail.com>
2020-10-04 21:37:04 -0400

    Initial Project Created
    Initial project created through XCode.

---
+++ XRose/XRose/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XRose
+//
+//  Created by Sammy Alvarado on 10/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
